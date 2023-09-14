2a35fe8400ba23b3b58d0c86b4ffdf87b496f3e3
Urvish <myappradar@gmail.com>
2023-09-15 00:45:40 +0530

    chat app

---
+++ client/src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Row, Col, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import "./Home.css";
+
+function Home() {
+    return (
+        <Row>
+            <Col md={6} className="d-flex flex-direction-column align-items-center justify-content-center">
+                <div>
+                    <h1>Raft Labs Node Js Assignment</h1>
+                    <p>Task 02</p>
+                    <LinkContainer to="/chat">
+                        <Button color="#01C247">
+                            Get Started <i className="fas fa-comments home-message-icon"></i>
+                        </Button>
+                    </LinkContainer>
+                </div>
+            </Col>
+            <Col md={6} className="home__bg"></Col>
+        </Row>
+    );
+}
+
+export default Home;
---
+++ connection.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect('mongodb+srv://urvishmakwana2427:eami8mbG4pcnF3mf@cluster0.vjki0th.mongodb.net/?retryWrites=true&w=majority', ()=> {
+  console.log('connected to mongodb')
+});
+
+// mongodb
+// urvishmakwana2427
+// eami8mbG4pcnF3mf
+
+// cloudinary
+//noyoka8480@tenjb.com
+//Cnoyoka8480@tenjb.com
